410761f3ec72c3784cfda2ac5f092659c186cee0
Richard Grand'Maison <rich.grandmaison@gmail.com>
2019-09-16 23:15:51 -0400

    Link backend and front-end

--- backend/src/main/java/com/soen6461/rental/RentalApplication.java
+++ backend/src/main/java/com/soen6461/rental/RentalApplication.java
@@ -2,12 +2,25 @@ package com.soen6461.rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class RentalApplication {
+public class RentalApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentalApplication.class, args);
 	}
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/client/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
 
 }
