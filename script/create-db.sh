332e5cb291ebd52229d7dff53eb8595f2c63bff2
Richard Grand'Maison <rich.grandmaison@gmail.com>
2019-09-18 21:14:55 -0400

    Add password to create and destroy scripts, update readme and add init sql scripts

--- script/create-db.sh
+++ script/create-db.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-DB_USERNAME=rental
+DB_USERNAME=dbuser
 DB_PASSWORD=cc1234
 DB_DATABASE=soen6461
 
-MYSQL_OPT="--host=localhost --user=root --password="
+echo "Enter your mysql username:"
+read MYSQL_USER
+echo "Enter your mysql password:"
+read MYSQL_PASSWORD
+
+MYSQL_OPT="--host=localhost --user=${MYSQL_USER} --password=${MYSQL_PASSWORD}"
 
 echo "create database ${DB_DATABASE} DEFAULT CHARACTER SET utf8;" | mysql ${MYSQL_OPT}
 echo "create user ${DB_USERNAME} identified by '${DB_PASSWORD}';" | mysql ${MYSQL_OPT}
--- script/destroy-db.sh
+++ script/destroy-db.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-DB_USERNAME=rental
+DB_USERNAME=dbuser
 DB_PASSWORD=cc1234
 DB_DATABASE=soen6461
 
-MYSQL_OPT="--host=localhost --user=root --password="
+echo "Enter your mysql username:"
+read MYSQL_USER
+echo "Enter your mysql password:"
+read MYSQL_PASSWORD
+
+MYSQL_OPT="--host=localhost --user=${MYSQL_USER} --password=${MYSQL_PASSWORD}"
 
 echo "drop user ${DB_USERNAME};" | mysql ${MYSQL_OPT}
 echo "drop database ${DB_DATABASE};;" | mysql ${MYSQL_OPT}
