4a93c86f2e17e930db785765741d9af65cf07787
Richard Grand'Maison <rich.grandmaison@gmail.com>
2019-09-28 18:21:48 -0400

    Set up all pages routes

--- client/src/app/routing.module.ts
+++ client/src/app/routing.module.ts
@@ -4,6 +4,10 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {LogInComponent} from "./login/log-in.component";
 import {HomeComponent} from "./home/home.component";
 import {VehicleCatalogComponent} from "./vehicle-catalog/vehicle-catalog.component";
+import {ReturnsComponent} from "./returns/returns.component";
+import {ReservationsComponent} from "./reservations/reservations.component";
+import {RentalsComponent} from "./rentals/rentals.component";
+import {ClientRecordsComponent} from "./client-records/client-records.component";
 
 const routes: Routes = [
   {
@@ -27,6 +31,23 @@ const routes: Routes = [
       {
         path: 'catalog',
         component: VehicleCatalogComponent
+      },
+      {
+        path: 'client-records',
+        component: ClientRecordsComponent
+      },
+      {
+        path: 'rentals',
+        component: RentalsComponent
+      },
+      {
+        path: 'reservations',
+        component: ReservationsComponent
+      }
+      ,
+      {
+        path: 'returns',
+        component: ReturnsComponent
       }
     ]
   }
