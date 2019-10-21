cf302327038d48d819fd0858ac31469568c3dc94
pargol <pargol_pd@yahoo.com>
2019-10-20 20:34:49 -0400

    start transactiob history page

--- client/src/app/transactions/transactions.component.ts
+++ client/src/app/transactions/transactions.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { TransactionApiService, Transaction, Rental, Reservation } from '../api/transaction-api.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-transactions',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionsComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['licensePlate', 'driverLicense', 'type', 'startDate', 'dueDate', 'returnDate', 'cancelDate'];
+
+  licensePlate = new FormControl();
+  driverLicense = new FormControl();
+  type = new FormControl();
+  startDate = new FormControl();
+  dueDate = new FormControl();
+  returnDate = new FormControl();
+  cancelDate = new FormControl();
+
+  dataSource: MatTableDataSource<Transaction>;
+
+  constructor(private transactionApiService: TransactionApiService ) { }
 
   ngOnInit() {
+    const rentals: Rental[] = this.transactionApiService.getRentals();
+    const reservations: Reservation[] = this.transactionApiService.getReservations();
+
+    this.dataSource = new MatTableDataSource<Transaction>();
+    rentals.forEach(r => this.dataSource.data.push(r));
+    reservations.forEach(r => this.dataSource.data.push(r));
   }
 
 }
