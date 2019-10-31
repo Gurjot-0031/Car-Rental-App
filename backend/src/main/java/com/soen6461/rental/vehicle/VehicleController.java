60b114ff46f9cb3f4e1c29a8870fe9cdc296f5b3
richbighouse <rgrandmaison@stack8.com>
2019-10-31 16:39:28 -0400

    Prep vehicle source

--- backend/src/main/java/com/soen6461/rental/vehicle/VehicleController.java
+++ backend/src/main/java/com/soen6461/rental/vehicle/VehicleController.java
@@ -1,6 +1,8 @@
 package com.soen6461.rental.vehicle;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +19,11 @@ public class VehicleController {
     @GetMapping("/api/vehicles")
     public List<Vehicle> getAllVehicles() {
         return vehicleService.getAllVehicles();
+    }
+
+    @PostMapping("/api/vehicle")
+    public void createVehicle(@RequestBody Vehicle vehicle) {
+        vehicleService.createVehicle(vehicle);
     }
 }
 
--- backend/src/main/java/com/soen6461/rental/vehicle/VehicleService.java
+++ backend/src/main/java/com/soen6461/rental/vehicle/VehicleService.java
@@ -1,6 +1,10 @@
 package com.soen6461.rental.vehicle;
 
+import com.google.common.collect.ImmutableMap;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -24,6 +28,28 @@ public class VehicleService {
                 (rs, rowNum) -> mapResultSetToVehicle(rs)
             );
     }
+
+
+    public void createVehicle(Vehicle vehicle) {
+        //language=SQL
+        String sql = "INSERT INTO Vehicle (type, make, model, color, license, year) \n" +
+            "VALUES (:type, :make, :model, :color, :license, :year);";
+
+        new NamedParameterJdbcTemplate(dataSource)
+            .query(
+                sql,
+                (SqlParameterSource) ImmutableMap.builder()
+                    .put("type", vehicle.getType())
+                    .put("make", vehicle.getMake())
+                    .put("model", vehicle.getModel())
+                    .put("color", vehicle.getColor())
+                    .put("license", vehicle.getLicense())
+                    .put("year", vehicle.getYear())
+                    .build(),
+                (rs, rowNum) -> mapResultSetToVehicle(rs)
+            );
+    }
+
 
     private Vehicle mapResultSetToVehicle(ResultSet rs) throws SQLException {
         Vehicle vehicle = new Vehicle();
